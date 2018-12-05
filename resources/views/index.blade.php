f9c2401552520cf0b46c10e2575f89dd55457025
Ali Delkhosh <ali_delkhosh@ymail.com>
2018-12-06 00:03:39 +0330

    index.php changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,15 +41,6 @@
       gtag('config', 'UA-129398000-1');
     </script>
 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129398000-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-129398000-1');
-    </script>
-
 </head>
 
 <body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
