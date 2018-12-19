c5fe38294078d1e13d3ca11e85927e582beff03c
Ali <ali_delkhosh@ymail.com>
2018-12-19 16:48:02 +0330

    still workin on enamad

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -511,11 +511,9 @@ FAQ SECTION
             </div>
 <!--            @if(Request::url() == 'localhost:6060' || Request::url() == 'www.incobac.ir' || Request::url() == 'https:://incobac.ir' || Request::url() == 'https:://www.incobac.ir')-->
                 
-                <div class="col-xs-12 col-sm-6 col-md-4 enamad">
-                    <img src="https://trustseal.enamad.ir/logo.aspx?id=100286&amp;p=yBEoWWMXqCeIkdjL" alt=""
-                         onclick="window.open(&quot;https://trustseal.enamad.ir/Verify.aspx?id=100286&amp;p=yBEoWWMXqCeIkdjL&quot;, &quot;Popup&quot;,&quot;toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30&quot;)"
-                         style="cursor:pointer" id="yBEoWWMXqCeIkdjL">
-                </div>
+<!--                <div class="col-xs-12 col-sm-6 col-md-4 enamad">-->
+                    <img src="https://trustseal.enamad.ir/logo.aspx?id=100286&amp;p=JkEP1tFdjs5Vc9mS" alt=""           onclick="window.open(&quot;https://trustseal.enamad.ir/Verify.aspx?id=100286&amp;p=JkEP1tFdjs5Vc9mS&quot;, &quot;Popup&quot;,&quot;toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30&quot;)" style="cursor:pointer" id="JkEP1tFdjs5Vc9mS">
+<!--                </div>-->
 <!--            @elif(Request::url() == 'incobac.com' || Request::url() == 'www.incobac.com' || Request::url() == 'https:://incobac.com' || Request::url() == 'https:://www.incobac.com')-->
                 <div class="col-xs-12 col-sm-6 col-md-4 enamad">
                     <img src="https://trustseal.enamad.ir/logo.aspx?id=110135&amp;p=e4AvUmJtRgJZ9BOl" alt="" onclick="window.open(&quot;https://trustseal.enamad.ir/Verify.aspx?id=110135&amp;p=e4AvUmJtRgJZ9BOl&quot;, &quot;Popup&quot;,&quot;toolbar=no, location=no, statusbar=no, menubar=no, scrollbars=1, resizable=0, width=580, height=600, top=30&quot;)" style="cursor:pointer" id="e4AvUmJtRgJZ9BOl">
