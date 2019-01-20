9f53723eaa5fd0dedf621434c431ae1086d5dffe
Ali <ali_delkhosh@ymail.com>
2019-01-20 11:23:23 +0330

    traction pages default text have been added

--- resources/assets/js/components/dashboard/seller/transaction/my-transactions.vue
+++ resources/assets/js/components/dashboard/seller/transaction/my-transactions.vue
@@ -209,7 +209,7 @@
               <div class="loading_images  col-xs-12" v-else-if="isLoading">
                   <img :src="loading" style="width:200px;height:200px">
               </div>
-              <div class="col-xs-12" v-else>
+              <div class="col-xs-12" v-else-if="transactions.length == 0 && instantTransactions.length == 0">
                   <h4 class="text-center" dir="rtl">تراکنشی در این قسمت وجود ندارد.</h4>
               </div>
         </section>
