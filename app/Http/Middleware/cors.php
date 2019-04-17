52d1a70ab782612de817e60a68e52121b55db0d1
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-04-17 17:03:11 +0430

    same shit

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -15,10 +15,22 @@ class cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: https://www.incobac.com');
-        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
-        
-        return $next($request);
+//        header('Access-Control-Allow-Origin: https://www.incobac.com');
+//        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'https://www.incobac.com');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
+
+        return $response;
+//        $request->withHeaders([
+//            'Access-Control-Allow-Origin' => 'https://www.incobac.com',
+//            'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+//            'Access-Control-Allow-Headers' => 'Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token'
+//        ]);
+//        
+//        return $next($request);
     }
 }
