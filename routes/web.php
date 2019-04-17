204ea0524c68017acbe29123f0a9b16d1d6ed657
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-04-17 18:02:59 +0430

    working on web.php

--- routes/web.php
+++ routes/web.php
@@ -908,7 +908,7 @@ Route::any('/external-url-payment-callback',[
 
 Route::group(['middleware' => [cors::class]],function(){
     Route::options('/broadcastAuth',function(){
-        return response()
+        return response('OK', \Illuminate\Http\Response::HTTP_NO_CONTENT)
             ->header('Access-Control-Allow-Origin','*')
             ->header('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers','Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
