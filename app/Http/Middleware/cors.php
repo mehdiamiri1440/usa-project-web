8543607f079819aecebbe1e575e628b9727baa30
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-04-17 16:46:20 +0430

    same shit

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -15,7 +15,7 @@ class cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: https://incobac.com');
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
         
