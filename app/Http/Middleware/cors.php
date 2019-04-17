278dd06104bf8ad29f96b281015d6a7ed3654123
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-04-17 16:40:29 +0430

    working on the same shit still

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -17,7 +17,7 @@ class cors
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application','x-csrf-token');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
         
         return $next($request);
     }
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -32,7 +32,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    console.log(token.content);
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
