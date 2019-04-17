6327afafdce667540c2b7f9c38f6b38b15a47c0d
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-04-17 18:14:33 +0430

    changing cors middleware

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -15,9 +15,9 @@ class cors
      */
     public function handle($request, Closure $next)
     {
-//        header('Access-Control-Allow-Origin: https://www.incobac.com');
-//        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, X-CSRF-Token');
 //        $response = $next($request);
 //
 //        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
@@ -33,9 +33,9 @@ class cors
 //        
         
 
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
+//        header('Access-Control-Allow-Origin: *');
+//        header('Access-Control-Allow-Methods: *');
+//        header('Access-Control-Allow-Headers: *');
 
         return $next($request);
     }
