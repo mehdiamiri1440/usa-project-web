f57492e22022a0f9db07dcf4c71e0036056944e3
Ali Delkhosh <ali_delkhosh@ymail.com>
2019-08-14 17:12:50 +0430

    compiled in production mode

--- storage/framework/views/9b9954198a945d9ab23dee268f3a3554ea0f6d1a.php
+++ storage/framework/views/9b9954198a945d9ab23dee268f3a3554ea0f6d1a.php
@@ -17,8 +17,9 @@
     <script src="<?php echo e(asset('assets/js/jquery-3.3.1.min.js')); ?>"></script>
     <script src="<?php echo e(asset('assets/js/jquery-ui.min.js')); ?>"></script>
     
-    
+    <script src="<?php echo e(asset('assets/js/jquery.easing.min.js')); ?>"></script>
 
+    <link type="text/css" rel="stylesheet" href="<?php echo e(asset('assets/css/placeholder-content.css')); ?>">
     <link type="text/css" rel="stylesheet" href="<?php echo e(asset('assets/css/fontiran.css')); ?>">
     <link type="text/css" rel="stylesheet" href="<?php echo e(asset('assets/css/font-awesome.min.css')); ?>">
     <link type="text/css" rel="stylesheet" href="<?php echo e(asset('assets/css/bootstrap.min.css')); ?>">
@@ -34,7 +35,7 @@
     <link type="text/css" rel="stylesheet" href="<?php echo e(asset('assets/css/owl-custom-style.css')); ?>">
 
 
-    
+    <script src="<?php echo e(asset('index/js/jquery-ui.min.js')); ?>"></script>
     <script src="<?php echo e(asset('assets/cdn/axios.js')); ?>"></script>
 
 
