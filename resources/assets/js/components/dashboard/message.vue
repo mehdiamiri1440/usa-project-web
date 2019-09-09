84f0152025ac63cedfdeb4d98589b6f3a92819ab
Mohammad Amin <contact.deldari@gmail.com>
2019-09-09 13:02:58 +0430

    change ui design

--- resources/assets/js/components/dashboard/message.vue
+++ resources/assets/js/components/dashboard/message.vue
@@ -428,11 +428,13 @@
 
 .send-message-form .button-wrapper button {
   border: none;
-  background: #fff;
+  background: #00c569;
   width: 50px;
   height: 50px;
   border-radius: 50px;
-  font-size: 25px;
+  font-size: 22px;
+  color: #fff;
+  padding-left: 5px;
 }
 
 .contact-not-found {
