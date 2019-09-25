aeaa7f99b41c4dff7b9c24b93cd7f088c5d1d713
Mohammad Amin <contact.deldari@gmail.com>
2019-09-25 18:21:48 +0330

    add landing page and developed

--- storage/framework/views/4d14c2a3afbbfd1e3c613656e7a7cf2a60441e5b.php
+++ storage/framework/views/4d14c2a3afbbfd1e3c613656e7a7cf2a60441e5b.php
@@ -37,10 +37,8 @@
     <script src="<?php echo e(asset('assets/cdn/axios.js')); ?>"></script>
 
     <script src="<?php echo e(asset('index/js/jquery.magnific-popup.min.js')); ?>"></script>
-    <script src="<?php echo e(asset('index/js/jquery.parallax.js')); ?>"></script>
     <script src="<?php echo e(asset('index/js/owl.carousel.min.js')); ?>"></script>
-    <script src="<?php echo e(asset('index/js/smoothscroll.js')); ?>"></script>
-    <script src="<?php echo e(asset('index/js/wow.min.js')); ?>"></script>
+
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129398000-1"></script>
@@ -146,12 +144,6 @@
                  img_success_verified="<?php echo e(asset('index/images/farmer.png')); ?>"
                  img_seller_services="<?php echo e(asset('index/images/seller.jpg')); ?>"
                  img_buyer_services="<?php echo e(asset('index/images/buyer.jpg')); ?>"
-                 img_six="<?php echo e(asset('index/images/6.png')); ?>"
-                 img_five="<?php echo e(asset('index/images/5.png')); ?>"
-                 img_four="<?php echo e(asset('index/images/4.png')); ?>"
-                 img_three="<?php echo e(asset('index/images/3.png')); ?>"
-                 img_two="<?php echo e(asset('index/images/2.png')); ?>"
-                 img_one="<?php echo e(asset('index/images/1.png')); ?>"
                  img_2_help_page="<?php echo e(asset('assets/img/help/2.jpg')); ?>"
                  img_3_help_page="<?php echo e(asset('assets/img/help/3.jpg')); ?>"
                  img_4_help_page="<?php echo e(asset('assets/img/help/4.jpg')); ?>"
@@ -195,7 +187,6 @@
     });
 
     $(document).on("idle.idleTimer", function () {
-        // function you want to fire when the user goes idle
         window.location.href = '/login'
     });
 </script>
