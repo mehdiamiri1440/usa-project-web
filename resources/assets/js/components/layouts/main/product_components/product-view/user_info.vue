3b5aee2ae6911d7da7524cdf11e35b5f0356be98
Mohammad Amin <contact.deldari@gmail.com>
2019-12-09 21:49:22 +0330

    deug profile image in produt view page

--- resources/assets/js/components/layouts/main/product_components/product-view/user_info.vue
+++ resources/assets/js/components/layouts/main/product_components/product-view/user_info.vue
@@ -48,11 +48,13 @@ i {
 }
 
 .user-information-content-image img {
-  width: initial;
-  height: 100%;
+  width: 100%;
+  min-height: 100%;
   position: relative;
   left: 50%;
-  transform: translate(-50%, 0);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  top: 50%;
 }
 .valid-icon {
   background: #00c569;
