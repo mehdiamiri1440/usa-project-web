94eee8f5648b106934eeadfcbd0cddf270242098
Mohammad Amin <contact.deldari@gmail.com>
2020-01-18 15:20:18 +0330

    add content place holder to finish step register request

--- resources/assets/js/components/dashboard/buyer/request/register-request-steps/fnish-register-request.vue
+++ resources/assets/js/components/dashboard/buyer/request/register-request-steps/fnish-register-request.vue
@@ -2,7 +2,9 @@
 <style scoped>
 
 
-
+	.main-article-wrapper{
+        margin-bottom:35px ;
+    }
     h2{
     	font-size: 23px;
 		font-weight: bold;
@@ -27,6 +29,10 @@
 		padding: 0 50px;
 	}
 
+	.default-main-article{
+		background: #fff;
+	}
+
 	@media screen and (max-width: 1199px){
 		.main-content-wrapper{
 			padding: 0;
@@ -36,25 +42,57 @@
 
 <template>
 	<div>
-		<header class="header-contents-wrapper text-center text-rtl  col-xs-12">
-				<div class="header-contents shadow-content">
-					<h2>
-					<i class="fa fa-check green-text"></i>  درخواست شما با موفقیت ثبت شد
-					</h2>
-					<p class="main-text">
-						تعدادی از مرتبط ترین محصولات برای درخواست خرید شما در زیر نمایش داده شده است
+		
 
-					</p>
-					<p class="red-text ">
-						 در صورت عدم ارسال پیام به فروشنده ها دیگر به این محصولات دسترسی نخواهید داشت.
-					</p>
+		<div>
+			<header class="header-contents-wrapper text-center text-rtl  col-xs-12">
+					<div class="header-contents shadow-content">
+						<h2>
+						<i class="fa fa-check green-text"></i>  درخواست شما با موفقیت ثبت شد
+						</h2>
+						<p class="main-text">
+							تعدادی از مرتبط ترین محصولات برای درخواست خرید شما در زیر نمایش داده شده است
+
+						</p>
+						<p class="red-text ">
+							 در صورت عدم ارسال پیام به فروشنده ها دیگر به این محصولات دسترسی نخواهید داشت.
+						</p>
+					</div>
+			</header>
+
+			<main class="main-content-wrapper">
+
+				<finish-step-article v-for="(item , index) in 6" :key="index"/>
+
+				<div class="default-placeholder-content" v-for="(item , index) in 6" :key="index">
+					<article class="main-article-wrapper col-xs-12 col-sm-6 col-md-4 pull-right  ">
+			            <div class="default-main-article text-center shadow-content">
+			                <div class=" placeholder-content default-finish-step-image">
+			                    
+			                </div>
+
+			                <div class="default-main-content text-center  padding-10">
+			                    <p class="placeholder-content content-default-width margin-10-auto"></p>
+			                    <p class=" placeholder-content content-half-width margin-auto">
+			                       
+			                    </p>
+			                    <p class="placeholder-content default-button-full-with	"></p>
+			                </div>
+
+			            </div>
+			        </article>
 				</div>
-		</header>
+			</main>
 
-		<main class="main-content-wrapper">
+		</div>
 
-			<finish-step-article v-for="(item , index) in 6" :key="index"/>
-		</main>
+
+		
+
+
+
+
+
     </div>
 </template>
 
