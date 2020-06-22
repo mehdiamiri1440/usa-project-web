d7425963c110e7ecdffb30135c36c89a46cbbfe2
Ali Delkhosh <ali_delkhosh@ymail.com>
2020-06-23 00:02:26 +0430

    admin panel route for verification check added

--- resources/views/admin_panel/layout/master.blade.php
+++ resources/views/admin_panel/layout/master.blade.php
@@ -115,6 +115,9 @@
         <li class="{{Request::is('admin/suspicious-messages-list') ? 'active':''}}">
             <a href="{{route('admin_panel_load_messaging_anomaly')}}"><i class="fa fa-envelope"></i><span>آنومالی در پیام ها</span></a>      
         </li>
+        <li class="{{Request::is('admin/unverified-user-list') ? 'active':''}}">
+            <a href="{{route('admin_load_unverified_documents')}}"><i class="fa fa-envelope"></i><span>اطلاعات احراز هویت</span></a>      
+        </li>
         <!-- <li class="{{Request::is('admin/sell-offers') || Request::is('admin/sell-offer-detail/*') ? 'active':''}}">
             <a href="{{route('admin_panel_sell_offer_list')}}"><i class="fa fa-envelope"></i><span>پیشنهادات فروش</span></a>     
         </li>
