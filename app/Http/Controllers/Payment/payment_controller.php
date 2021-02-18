f405131b5bc1ba493170fe8bd2f84bf63a6e5f8f
mehdi amiri <knownmma@gmail.com>
2021-02-19 02:37:15 +0330

    changes back in pricing

--- app/Http/Controllers/Payment/payment_controller.php
+++ app/Http/Controllers/Payment/payment_controller.php
@@ -165,7 +165,8 @@ class payment_controller extends Controller
         } 
         catch (\Exception $e)
         {
-            return redirect('buskool://pricing');
+            return redirect('buskool://register-product-successfully');
+            // return redirect('buskool://pricing');
         }
     }
     
