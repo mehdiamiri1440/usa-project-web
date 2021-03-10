5b6984bebc7f5c26ee711413aaa6ed38c7aae6e8
MohammadAmin Deldari <contact.deldari@gmail.com>
2021-03-10 11:18:33 +0330

    debug products

--- resources/views/layout/product-list-components/main.blade.php
+++ resources/views/layout/product-list-components/main.blade.php
@@ -80,20 +80,20 @@
                       </div>
                       
                     @if($product['user_info']->active_pakage_type != 3)
-                    <div
-                        class="main-article-contents-wrapper pointer-class "
+                    <a target="_blank" href="{{'/product-view/' . $product['main']->id  .'/'. str_replace(' ', '-', $product['main']->category_name) . '/' . str_replace(' ', '-', 'خرید-عمده-' .$product['main']->sub_category_name)  }}"
+                      class="main-article-contents-wrapper pointer-class "
                     >
                     @else
-                    <div
+                    <a target="_blank" href="{{'/product-view/' . $product['main']->id  .'/'. str_replace(' ', '-', $product['main']->category_name) . '/' . str_replace(' ', '-', 'خرید-عمده-' .$product['main']->sub_category_name)  }}"
                     class="main-article-contents-wrapper pointer-class is-user-valid-content"
-                >
+                    >
                     @endif
                         <div class="main-article-contents-image-wrapper" >
                     
                             <div class="main-article-image">
-                              <a target="_blank" href="{{'/product-view/' . $product['main']->id . '/' . str_replace(' ', '-', 'خرید-عمده-' .$product['main']->sub_category_name) .'/'. str_replace(' ', '-', $product['main']->category_name)  }}">
+                              <div class="image" >
                                 <img src="{{url('/storage') . '/thumbnails/' . $product['photos'][0]->file_path}}"  :alt="alt" />
-                                </a>
+                                </div>
                             
                                 <div class="lds-ring">
                                 <div></div>
@@ -112,81 +112,82 @@
                         </div>
                         <div class="main-article-contents" >
                           @if($product['user_info']->active_pakage_type == 3)
-                          <div
-                            class="valid-user-badge"
-                        >
-                            <div class="wrapper-icon">
-                            <svg width="24.965" height="30.574" viewBox="0 0 24.965 30.574">
-                                <g
-                                id="buskool-icon"
-                                data-name="buskool"
-                                transform="translate(-273.1 -715.025)"
-                                >
-                                <path
-                                    id="Subtraction_1"
-                                    data-name="Subtraction 1"
-                                    d="M-1951.5,35.792a12.419,12.419,0,0,1-8.839-3.661A12.419,12.419,0,0,1-1964,23.292a12.361,12.361,0,0,1,1.378-5.71,12.614,12.614,0,0,1,3.679-4.333l3.175,3.175a7.967,7.967,0,0,0-3.732,6.768,8.009,8.009,0,0,0,8,8,8.036,8.036,0,0,0,7.917-6.85l2.185-2.149,2.34,2.3a12.464,12.464,0,0,1-4.012,8.026A12.467,12.467,0,0,1-1951.5,35.792Zm12.465-13.44,0,0-2.361-2.33-2.169,2.14a8.029,8.029,0,0,0-4.052-5.965l3.2-3.2a12.44,12.44,0,0,1,5.381,9.357Z"
-                                    transform="translate(2237.1 709.808)"
-                                    fill="#fff"
-                                />
-                                <g id="Group_24" data-name="Group 24">
-                                    <path
-                                    id="Rectangle_12"
-                                    data-name="Rectangle 12"
-                                    d="M3,0H9.5a0,0,0,0,1,0,0V5.5a0,0,0,0,1,0,0H0a0,0,0,0,1,0,0V3A3,3,0,0,1,3,0Z"
-                                    transform="translate(282.389 717.5) rotate(45)"
-                                    fill="#fff"
-                                    />
-                                    <path
-                                    id="Rectangle_13"
-                                    data-name="Rectangle 13"
-                                    d="M0,0H13.5a0,0,0,0,1,0,0V5a0,0,0,0,1,0,0H4A4,4,0,0,1,0,1V0A0,0,0,0,1,0,0Z"
-                                    transform="translate(294.935 718.561) rotate(135)"
-                                    fill="#fff"
-                                    />
-                                </g>
-                                </g>
-                            </svg>
+                            <div
+                              class="valid-user-badge"
+                            >
+                              <div class="wrapper-icon">
+                              <svg width="24.965" height="30.574" viewBox="0 0 24.965 30.574">
+                                  <g
+                                  id="buskool-icon"
+                                  data-name="buskool"
+                                  transform="translate(-273.1 -715.025)"
+                                  >
+                                  <path
+                                      id="Subtraction_1"
+                                      data-name="Subtraction 1"
+                                      d="M-1951.5,35.792a12.419,12.419,0,0,1-8.839-3.661A12.419,12.419,0,0,1-1964,23.292a12.361,12.361,0,0,1,1.378-5.71,12.614,12.614,0,0,1,3.679-4.333l3.175,3.175a7.967,7.967,0,0,0-3.732,6.768,8.009,8.009,0,0,0,8,8,8.036,8.036,0,0,0,7.917-6.85l2.185-2.149,2.34,2.3a12.464,12.464,0,0,1-4.012,8.026A12.467,12.467,0,0,1-1951.5,35.792Zm12.465-13.44,0,0-2.361-2.33-2.169,2.14a8.029,8.029,0,0,0-4.052-5.965l3.2-3.2a12.44,12.44,0,0,1,5.381,9.357Z"
+                                      transform="translate(2237.1 709.808)"
+                                      fill="#fff"
+                                  />
+                                  <g id="Group_24" data-name="Group 24">
+                                      <path
+                                      id="Rectangle_12"
+                                      data-name="Rectangle 12"
+                                      d="M3,0H9.5a0,0,0,0,1,0,0V5.5a0,0,0,0,1,0,0H0a0,0,0,0,1,0,0V3A3,3,0,0,1,3,0Z"
+                                      transform="translate(282.389 717.5) rotate(45)"
+                                      fill="#fff"
+                                      />
+                                      <path
+                                      id="Rectangle_13"
+                                      data-name="Rectangle 13"
+                                      d="M0,0H13.5a0,0,0,0,1,0,0V5a0,0,0,0,1,0,0H4A4,4,0,0,1,0,1V0A0,0,0,0,1,0,0Z"
+                                      transform="translate(294.935 718.561) rotate(135)"
+                                      fill="#fff"
+                                      />
+                                  </g>
+                                  </g>
+                              </svg>
+                              </div>
                             </div>
-                        </div>
                           @endif
                         
-                        <div>
-                            <h3 class="article-title">
-                              <a target="_blank" href="{{'/product-view/' . $product['main']->id . '/' . str_replace(' ', '-', 'خرید-عمده-' .$product['main']->sub_category_name) .'/'. str_replace(' ', '-', $product['main']->category_name)  }}">
-                                
-                                  {{$product['main']->category_name . ' | ' . $product['main']->sub_category_name  }}
-                                  <span>
-                                    {{$product['main']->product_name}}
-                                  </span>
-                              </a>
-                            </h3>
+                          <div>
+                              <h3 class="article-title">
+                                <p>
+                                  
+                                    {{$product['main']->category_name . ' | ' . $product['main']->sub_category_name  }}
+                                    <span>
+                                      {{$product['main']->product_name}}
+                                    </span>
+                                  </p>
+                              </h3>
 
-                            <p>
-                            استان / شهر:
-                            <span
-                            >
-                            {{$product['main']->province_name . ' - ' . $product['main']->city_name  }}
-                          </span>
-                            </p>
-
-                            <p  class="product-description">
-                            توضیحات
-                            <a target="_blank" href="{{'/product-view/' . $product['main']->id . '/' . str_replace(' ', '-', 'خرید-عمده-' .$product['main']->sub_category_name) .'/'. str_replace(' ', '-', $product['main']->category_name)  }}">
-                            {{$product['main']->description  }}
-                          </a>
-                            </p>
-
-                            <p>
-                            مقدار موجودی:
-                            <span >
-                              {{$product['main']->stock  }} کیلوگرم
+                              <p>
+                              استان / شهر:
+                              <span
+                              >
+                              {{$product['main']->province_name . ' - ' . $product['main']->city_name  }}
                             </span>
-                            </p>
-                        </div>
+                              </p>
 
-                        </div>
-                    </div>
+                              <p  class="product-description">
+                              توضیحات
+
+                              <span>
+                                {{$product['main']->description  }}
+                              </span>
+
+                              </p>
+
+                              <p>
+                              مقدار موجودی:
+                              <span >
+                                {{$product['main']->stock  }} کیلوگرم
+                              </span>
+                              </p>
+                          </div>
+                      </div>
+                    </a>
                 
                     <div class="footer-article">
                       @if($product['main']->is_elevated)
