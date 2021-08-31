35b3a286a1bc3e18c4d8cbf0982cbe3baf4d9566
Reza <merezarezaei@gmail.com>
2021-08-31 16:24:04 +0430

    some code refactor sms controller

--- app/Http/Controllers/Notification/sms_controller.php
+++ app/Http/Controllers/Notification/sms_controller.php
@@ -91,7 +91,6 @@ class sms_controller extends Controller
 		
     }
 	
-	
 	protected function generate_random_number()
 	{
 		return rand(1000,9999);
@@ -105,6 +104,7 @@ class sms_controller extends Controller
          ]); 
     }
 	
+    ///////////////////////////
 	public function verify_code(Request $request)
 	{
 		$rules = [
@@ -172,70 +172,7 @@ class sms_controller extends Controller
 		}
 	}
     
-    
-    //Ads messages
-    public function send_status_sms_message(&$db_record,$status_message,$url_prefix = null)
-    {
-        $user_id = $db_record->myuser_id;
-        $db_record_id = $db_record->id;
-        
-        $user_record = myuser::find($user_id);
-            
-        $message_array[] = $this->get_user_full_name_with_related_prefix($user_record);
-
-        $user_phone = $user_record->phone;
-
-        $message_array[] = $status_message ;
-
-        if( ! is_null($url_prefix)){
-            $record_link = url($url_prefix."/".$db_record_id);
-            $message_array[] = $record_link;
-        }
-        
-        $sms_message_text = $this->create_text_message($message_array);
-
-        try{
-            Smsir::sendToCustomerClub($sms_message_text,$user_phone);
-        }
-        catch(\Exception $e){
-            //
-        }
-        
-    }
-    protected function create_text_message($message_array_to_concat_with_blank_line_between_elements)
-    {
-        return implode("\n",$message_array_to_concat_with_blank_line_between_elements)."\n";
-    }
-    
-    protected function get_user_full_name_with_related_prefix(&$user_record)
-    {
-        if($user_record->sex == $this->sex_array[0]){
-            $prefix = 'جناب ' . $this->sex_array[0] . 'ی';
-        }
-        else if($user_record->sex == $this->sex_array[1]){
-            $prefix = 'سرکار ' . $this->sex_array[1] ;
-        }
-        else{
-            $prefix = '';
-        }
-        
-        $full_name = $user_record->first_name . ' ' . $user_record->last_name ; 
-        
-        return $prefix . ' ' . $full_name ;
-    }
-    
-    protected function generate_plain_text_password($password_len)
-    {
-        $result = "";
-        $chars = "abcdefghijkmnpqrstuvwxyzABCDEFGH123456789";
-        $charArray = str_split($chars);
-        for($i = 0; $i < $password_len; $i++){
-            $randItem = array_rand($charArray);
-            $result .= "".$charArray[$randItem];
-        }
-        return $result;
-    }
-    
+    // used in other places 
     public function send_new_generated_password($password,$user_phone)
     {
         try{
@@ -246,11 +183,7 @@ class sms_controller extends Controller
         }
     }
     
-    public function add_to_customer_club($first_name,$last_name,$phone)
-    {
-        Smsir::addToCustomerClub('',$first_name,$last_name,$phone);
-    }
-    
+    /////////////////////////// three following functions used in other places
     public function send_notify_sms_to_user($msg_array,$user_id)
     {
         $final_msg = $this->create_text_message($msg_array);
@@ -281,29 +214,6 @@ class sms_controller extends Controller
         else return false;
     }
     
-    public function send_notify_sms_to_phone_number($msg_array,$phone_number)
-    {
-        $final_msg = $this->create_text_message($msg_array);
-        
-        try{
-            Smsir::sendToCustomerClub($final_msg,$phone_number);
-            return $phone_number;
-        }
-        catch(\Exception $e){
-            //
-        }
-    }
-    
-    public function send_new_message_sms_notification($phone_number)
-    {
-        try{
-            Smsir::ultraFastSend(['name' => 'کاربر'],20691,$phone_number);
-        }
-        catch(\Exception $e){
-            //
-        }
-    }
-    
     public function send_sms_to_given_phone_number($phone_number,$pattern_code,$data = [])
     {
         $user_first_name = DB::table('myusers')
@@ -329,4 +239,102 @@ class sms_controller extends Controller
             echo $e->getMessage();
         }
     }
+
+    /////////////////////// incommon functions
+
+    protected function create_text_message($message_array_to_concat_with_blank_line_between_elements)
+    {
+        return implode("\n",$message_array_to_concat_with_blank_line_between_elements)."\n";
+    }
+
+    /////////////////////// zombie functions
+
+    public function add_to_customer_club($first_name,$last_name,$phone)
+    {
+        Smsir::addToCustomerClub('',$first_name,$last_name,$phone);
+    }
+
+    //Ads messages
+    // zombie function 
+    public function send_status_sms_message(&$db_record,$status_message,$url_prefix = null)
+    {
+        $user_id = $db_record->myuser_id;
+        $db_record_id = $db_record->id;
+        
+        $user_record = myuser::find($user_id);
+            
+        $message_array[] = $this->get_user_full_name_with_related_prefix($user_record);
+
+        $user_phone = $user_record->phone;
+
+        $message_array[] = $status_message ;
+
+        if( ! is_null($url_prefix)){
+            $record_link = url($url_prefix."/".$db_record_id);
+            $message_array[] = $record_link;
+        }
+        
+        $sms_message_text = $this->create_text_message($message_array);
+
+        try{
+            Smsir::sendToCustomerClub($sms_message_text,$user_phone);
+        }
+        catch(\Exception $e){
+            //
+        }
+        
+    }
+    
+    protected function get_user_full_name_with_related_prefix(&$user_record)
+    {
+        if($user_record->sex == $this->sex_array[0]){
+            $prefix = 'جناب ' . $this->sex_array[0] . 'ی';
+        }
+        else if($user_record->sex == $this->sex_array[1]){
+            $prefix = 'سرکار ' . $this->sex_array[1] ;
+        }
+        else{
+            $prefix = '';
+        }
+        
+        $full_name = $user_record->first_name . ' ' . $user_record->last_name ; 
+        
+        return $prefix . ' ' . $full_name ;
+    }
+    
+    protected function generate_plain_text_password($password_len)
+    {
+        $result = "";
+        $chars = "abcdefghijkmnpqrstuvwxyzABCDEFGH123456789";
+        $charArray = str_split($chars);
+        for($i = 0; $i < $password_len; $i++){
+            $randItem = array_rand($charArray);
+            $result .= "".$charArray[$randItem];
+        }
+        return $result;
+    }
+
+    public function send_notify_sms_to_phone_number($msg_array,$phone_number)
+    {
+        $final_msg = $this->create_text_message($msg_array);
+        
+        try{
+            Smsir::sendToCustomerClub($final_msg,$phone_number);
+            return $phone_number;
+        }
+        catch(\Exception $e){
+            //
+        }
+    }
+
+    public function send_new_message_sms_notification($phone_number)
+    {
+        try{
+            Smsir::ultraFastSend(['name' => 'کاربر'],20691,$phone_number);
+        }
+        catch(\Exception $e){
+            //
+        }
+    }
+
 }
