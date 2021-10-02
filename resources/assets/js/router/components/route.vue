5069cb005be3a668511360b6f26cbc5ff5680ea3
MohammadAmin Deldari <contact.deldari@gmail.com>
2021-10-02 14:36:55 +0330

    avoid show wallet in page load

--- resources/assets/js/router/components/route.vue
+++ resources/assets/js/router/components/route.vue
@@ -1366,9 +1366,9 @@ export default {
   },
   mounted() {
     // eventBus.$emit("globalVerifiedBadgeContents", this.verifiedUserContent);
-    document.addEventListener("DOMContentLoaded", function (event) {
-      document.getElementById("master-loader-wrapper").style.display = "none";
-    });
+    // document.addEventListener("DOMContentLoaded", function (event) {
+    //   document.getElementById("master-loader-wrapper").style.display = "none";
+    // });
     this.activateDownloadAppButton();
     $("#wallet-modal").on("show.bs.modal", (e) => {
       this.handleBackKeys();
