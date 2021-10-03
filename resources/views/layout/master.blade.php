21fcf26ba62a1935e3fb818cd579e6e3ab0eba33
MohammadAmin Deldari <contact.deldari@gmail.com>
2021-10-03 11:03:52 +0330

    debug master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,58 +1,46 @@
 <!DOCTYPE html>
 <html lang="fa">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <meta name="keywords" content="محصولات کشاورزی,خرید مستقیم صیفی,فروشگاه آنلاین کشاورزی,باسکول">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <meta name="csrf-token" content="{{csrf_token()}}">
-  @if(config('app.name') != 'Laravel')
-  <meta name="robots" content="noindex" />
-  @endif
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="محصولات کشاورزی,خرید مستقیم صیفی,فروشگاه آنلاین کشاورزی,باسکول">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    @if(config('app.name') != 'Laravel')
+    <meta name="robots" content="noindex" />
+    @endif
+
+    <link rel="icon" href="{{asset('assets/img/logo/mobile-logo.svg')}}">
+    
+    <!-- fonts -->
+    <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum)_Bold.woff2')}}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum).woff2')}}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum)_Medium.woff2')}}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{asset('assets/webfonts/fa-solid-900.woff2')}}" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="{{asset('assets/webfonts/fa-regular-400.woff2')}}" as="font" type="font/woff2" crossorigin>
   
-  <link rel="icon" href="{{asset('assets/img/logo/mobile-logo.svg')}}">
-  <!-- fonts -->
-  <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum)_Bold.woff2')}}" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum).woff2')}}" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="{{asset('assets/fonts/woff2/IRANSansWeb(FaNum)_Medium.woff2')}}" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="{{asset('assets/webfonts/fa-solid-900.woff2')}}" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="{{asset('assets/webfonts/fa-regular-400.woff2')}}" as="font" type="font/woff2" crossorigin>
-  <link rel="preload" href="{{asset('assets/css/fontiran.min.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
-  <link rel="preload" href="{{asset('assets/css/all.min.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
-  <link rel="preload" href="{{asset('css/app.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
-  <title>باسکول | بازار خرید و فروش عمده محصولات غذایی و کشاورزی ایران بدون واسطه</title>
+    <!-- Main Styles
+    // improve for blocking request for preload
+    -->
+    <link rel="preload" href="{{asset('assets/css/fontiran.min.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
+    <link rel="preload" href="{{asset('assets/css/all.min.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
+    <link rel="preload" href="{{asset('css/app.css')}}" as="style" onload="this.rel='stylesheet'" crossorigin>
+    <title>باسکول | بازار خرید و فروش عمده محصولات غذایی و کشاورزی ایران بدون واسطه</title>
+
+     <script>
+        window.Laravel = {csrfToken: '{{csrf_token()}}'}
+    </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129398000-1"></script>
     <script>
-      window.Laravel = {csrfToken: '{{csrf_token()}}'}
-  </script>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script  src="{{asset('assets/js/firebase/gtag.min.js')}}" async></script>
-  <script  src="{{asset('assets/js/firebase/firebase-app-7.13.0.min.js')}}" defer></script>
-  <script  src="{{asset('assets/js/firebase/firebase-messaging-7.13.0.min.js')}}" defer></script>
-
-  <script>
-    //  gtag configs
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-          dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'UA-129398000-1', {'send_page_view': false});
-
-      // firebase configs
-      var firebaseConfig = {
-      apiKey: "AIzaSyDHAonYamODsV034KRTjzd4_C_z4YpzaRo",
-      authDomain: "incobac-64ec9.firebaseapp.com",
-      databaseURL: "https://incobac-64ec9.firebaseio.com",
-      projectId: "incobac-64ec9",
-      storageBucket: "incobac-64ec9.appspot.com",
-      messagingSenderId: "946346601127",
-      appId: "1:946346601127:web:74e870ed6dd09f369bc982",
-      measurementId: "G-ZXCG6RTHC7"
-  };
-      firebase.initializeApp(firebaseConfig);
-      const messaging = firebase.messaging();
-  </script>
-
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-129398000-1', {'send_page_view': false});
+    </script>
   <style>
     .main-loader-wrapper {
       position: fixed;
@@ -63,38 +51,39 @@
       height: 100%;
       left: 0;
       top: 0;
-      }
-      .logo-main-loader {
-        display: inline-block;
-        position: absolute;
-        left: 50%;
-        top: 40%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-        line-height: 1.618;
-        width: 100%;
-        padding: 0 15px;
-      }
-
-      .logo-main-loader svg {
-          width: 60px;
-          height: 70px;
-      }
-      .main-loader-shape-wrapper {
-        position: absolute;
-        height: 70px !important;
-        bottom: 20%;
-        left: calc(50% - 23px);
-      }
-      .spinner-border{
-        width: 4.5rem;
-        height: 4.5rem;
-        color: #bbb;
-        border-width: 3px;
-      }
+    }
+    .logo-main-loader {
+      display: inline-block;
+      position: absolute;
+      left: 50%;
+      top: 40%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+      line-height: 1.618;
+      width: 100%;
+      padding: 0 15px;
+    }
+    .logo-main-loader svg {
+        width: 60px;
+        height: 70px;
+    }
+    .main-loader-shape-wrapper {
+      position: absolute;
+      height: 70px !important;
+      bottom: 20%;
+      left: calc(50% - 23px);
+    }
+    /* preloader image style*/
+    .spinner-border{
+      width: 4.5rem;
+      height: 4.5rem;
+      color: #bbb;
+      border-width: 3px;
+    }
   </style>
 </head>
 <body >
+
  <!-- #regex main loader -->
  <div id="master-loader-wrapper" class="main-loader-wrapper">
     <div class="logo-main-loader">
@@ -138,20 +127,43 @@
     </div>
   </div>
 
+  <!-- end regex main loader -->
+
   <div id="app">
     <route-component
       user-id = "{{session('user_id')}}"
       is-seller = "{{session('is_seller')}}"
       assets = "{{asset('')}}"
-      storage-path = "{{url('storage/')}}"
+      storage-path = "{{url('/storage')}}"
       profile-photo="{{session('profile_photo')}}"
       user-full-name="{{session('full_name')}}"
       user-logout-path="{{route('logout')}}"
     />
   </div>
 
+<script src="https://www.gstatic.com/firebasejs/7.13.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.13.0/firebase-messaging.js"></script> 
+
+<script>
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyDHAonYamODsV034KRTjzd4_C_z4YpzaRo",
+        authDomain: "incobac-64ec9.firebaseapp.com",
+        databaseURL: "https://incobac-64ec9.firebaseio.com",
+        projectId: "incobac-64ec9",
+        storageBucket: "incobac-64ec9.appspot.com",
+        messagingSenderId: "946346601127",
+        appId: "1:946346601127:web:74e870ed6dd09f369bc982",
+        measurementId: "G-ZXCG6RTHC7"
+    };
+    // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        const messaging = firebase.messaging();
+</script>
+
 <script src="{{asset('js/app.js')}}" defer></script>
-<script src="{{asset('assets/js/custom.bootstrap.min.js')}}" defer></script>
+
 <script>
 
     function getUserId(){
@@ -164,6 +176,5 @@
     }
 
 </script>
-
 </body>
 </html>
